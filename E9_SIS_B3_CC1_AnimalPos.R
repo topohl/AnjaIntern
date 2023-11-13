007c0f9d3dd32b9aaaa417a8a93848b2fd3c1b64
anja7777 <100579985+anja7777@users.noreply.github.com>
2023-11-13 14:46:06 +0100

    reading and sorting of csv data in tibble
    deleted unnecessary columns, separated date and time

--- E9_SIS_B3_CC1_AnimalPos.R
+++ E9_SIS_B3_CC1_AnimalPos.R
@@ -4,15 +4,28 @@
 ##
 
 # libraries
+library(readr)        # load readr package for reading csv files
+library(stringr)
 
 
-# read exil file in tibble
+# paths
+fileSourcePath <-  "S:/Lab_Member/Anja/Git/AnjaIntern/E9_SIS_B3_CC1_AnimalPos.csv"
+
+
+# read csv file in tibble
+
+overallData <- read_delim(fileSourcePath,delim = ";", show_col_types = FALSE)
 
 
 # normalization
-
 # delete unnecessary columns
-
-# seperate date and time
-
+overallData <- select(overallData, -c(RFID, AM, zPos))
+# separate date and time into extra columns
+overallData[c('Date', 'Time')] <- str_split_fixed(overallData$DateTime, ' ', 2)
+#sort columns
+overallData <- overallData[c('Date', 'Time', 'Animal', 'xPos', 'yPos')]
 # 2NF
+
+
+
+# plot a grid with all the points 
