6e17c00c009e193786295870ab7d0ae84b2a5de9
anja7777 <100579985+anja7777@users.noreply.github.com>
2023-11-13 12:00:29 +0100

    preparations for position analysis

---
+++ E9_SIS_B3_CC1_AnimalPos.R
@@ -0,0 +1,18 @@
+## 11/2023
+## Anja Magister
+## ANALYSIS OF ANIMAL POSITIONS ##
+##
+
+# libraries
+
+
+# read exil file in tibble
+
+
+# normalization
+
+# delete unnecessary columns
+
+# seperate date and time
+
+# 2NF
