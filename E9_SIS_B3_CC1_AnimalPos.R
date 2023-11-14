6d4d547f14b1acc41461622fa927666240cf119d
anja7777 <100579985+anja7777@users.noreply.github.com>
2023-11-14 11:53:51 +0100

    round dateTime

--- E9_SIS_B3_CC1_AnimalPos.R
+++ E9_SIS_B3_CC1_AnimalPos.R
@@ -7,6 +7,7 @@
 library(readr)        # load readr package for reading csv files
 library(stringr)
 library(dplyr)
+library(lubridate)    # for rounding time
 
 
 # paths
@@ -21,15 +22,17 @@ overallData <- read_delim(fileSourcePath,delim = ";", show_col_types = FALSE)
 # normalization
 # delete unnecessary columns
 overallData <- select(overallData, -c(RFID, AM, zPos))
+# convert the DateTime column to a datetime format(also rounds the DateTime)
+overallData$DateTime <- as.POSIXct(overallData$DateTime, format = "%d.%m.%Y %H:%M:%S")
+
 # separate date and time into extra columns
 overallData[c('Date', 'Time')] <- str_split_fixed(overallData$DateTime, ' ', 2)
 # sort columns
 overallData <- overallData[c('Date', 'Time', 'Animal', 'xPos', 'yPos')]
 
-# round time on 100 seconds
 # 2NF
 
-
+###########################################################################################
 
 # plot a grid with all the points 
 xPositions <- overallData %>% pull(xPos)               
@@ -60,3 +63,14 @@ firstAnimalData <- overallData%>%
 xPositionsfirstA <- firstAnimalData %>% pull(xPos)               
 yPositionsfirstA <- firstAnimalData %>% pull(yPos)  
 plot(xPositionsfirstA, yPositionsfirstA)
+
+# grid to see the corners (8 coils)
+#vertical
+abline(v=(seq(0,300,100)), col="lightgray", lty="dotted")
+#horizontal
+abline(h=(seq(0,116,58)), col="lightgray", lty="dotted")
+
+##test
+x <- c("22:33:21.23","21:34:00.54")
+round(x,unit="seconds")
+round_date(x,unit="seconds")
