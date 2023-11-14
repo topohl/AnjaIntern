6e9d9efc4fe2b01e1553145b3519ae7cea46c51b
anja7777 <100579985+anja7777@users.noreply.github.com>
2023-11-14 18:00:18 +0100

    adding extra tibble,some tests

--- E9_SIS_B3_CC1_AnimalPos.R
+++ E9_SIS_B3_CC1_AnimalPos.R
@@ -27,12 +27,77 @@ overallData$DateTime <- as.POSIXct(overallData$DateTime, format = "%d.%m.%Y %H:%
 
 # separate date and time into extra columns
 overallData[c('Date', 'Time')] <- str_split_fixed(overallData$DateTime, ' ', 2)
+
+
+
 # sort columns
 overallData <- overallData[c('Date', 'Time', 'Animal', 'xPos', 'yPos')]
 
-# 2NF
+# 2NF m&3NF
+### create extra tibbles out of overallData: ###
+# Dates
+unique_dates <- unique(overallData$Date)
+Dates_tibble <- tibble(DateID = c(1:length(unique_dates)), Date = unique_dates)
+
+# Times
+# Generate a sequence of times from 00:00:00 to 23:59:59
+times_vector <- seq(from = as.POSIXct("00:00:00", format = "%H:%M:%S"), to = as.POSIXct("23:59:59", format = "%H:%M:%S"), by = "1 sec")
+# Convert the times to the desired format "hh:mm:ss" (Date excluded)
+times_vector <- format(times_vector, format = "%H:%M:%S")
+
+# create a tibble of every possible time in 24h with seconds(one day has 86400 seconds)
+Times_tibble <- tibble(TimeID = c(1:86400), Time = times_vector)
+
+#Animals
+unique_animals <- unique(overallData$Animal)
+Animals_tibble <- tibble(AnimalID = c(1:length(unique_animals)), Animal = unique_animals)
+
+#Positions
+positions <- select(overallData, c(xPos,yPos))
+unique_positions <- unique(positions)
+Positions_tibble <- tibble(PositionID = c(1:length(unique_positions$xPos)), xPos = unique_positions[1], yPos = unique_positions[2])
+
+# convert xPos and yPos to PositionsID(from Positions_tibble) in overallData
+
+
+# convert Animal Number to AnimalID(from Animals_tibble) in overallData
+overallData_animanls <- overallData%>%
+  left_join(Animals_tibble, by = c("Animal")) %>%
+  mutate(AnimalID = ifelse(is.na(AnimalID), "Unknown", as.character(AnimalID)))
+#######################################
+library(dplyr)
+
+# Beispiel-Datensätze (Ersetzen Sie diese mit Ihren eigenen Daten)
+coordinates_table <- tibble(
+  x = c(10, 20, 30),
+  y = c(15, 25, 35)
+)
+
+id_mapping_table <- tibble(
+  ID = c(1, 2, 3),
+  x = c(10, 20, 30),
+  y = c(15, 25, 35)
+)
+
+# Zusammenführen der Tabellen basierend auf den Koordinaten
+result <- coordinates_table %>%
+  left_join(id_mapping_table, by = c("x", "y")) %>%
+  mutate(ID = ifelse(is.na(ID), "Unknown", as.character(ID)))  # Wenn keine Übereinstimmung gefunden wurde, als "Unknown" markieren oder entsprechend anpassen
+
+# Ausgabe des Ergebnisses
+print(result)
 
 ###########################################################################################
+# test to acces special table rows...
+testTime = as.POSIXct("12:24:52", format = "%H:%M:%S")
+testTime <- format(testTime, format = "%H:%M:%S")
+testAnimal="OR414_sys.3"
+specific_row <- overallData%>%
+  filter(Time==testTime, Animal==testAnimal)
+print(specific_row)
+
+firstSpotInCage <- overallData%>%
+  filter(Date=="2023-04-24", Time== testTime)
 
 # plot a grid with all the points 
 xPositions <- overallData %>% pull(xPos)               
