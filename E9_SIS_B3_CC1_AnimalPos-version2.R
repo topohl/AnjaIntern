617579ce9729ff2bd4b73d41ecca33f9787520ea
anja7777 <100579985+anja7777@users.noreply.github.com>
2023-11-21 12:36:36 +0100

    new file, new idea, new algorithm start
    try: without every second entry

---
+++ E9_SIS_B3_CC1_AnimalPos-version2.R
@@ -0,0 +1,109 @@
+## 11/2023
+## Anja Magister
+## ANALYSIS OF ANIMAL POSITIONS - VERSION2 ##
+##
+
+# libraries
+library(readr)        # load readr package for reading csv files
+library(stringr)
+library(dplyr)
+library(lubridate)    # for rounding time
+library(tibble)       #important for tibble operations
+
+
+# paths
+fileSourcePath <-  "S:/Lab_Member/Anja/Git/AnjaIntern/E9_SIS_B3_CC1_AnimalPos.csv"
+
+
+# read csv file in tibble
+
+overallData <- read_delim(fileSourcePath,delim = ";", show_col_types = FALSE)
+
+####################################################################################################################################
+# organization of overallData:
+# delete unnecessary columns
+overallData <- select(overallData, -c(RFID, AM, zPos))
+# convert the DateTime column to a datetime format(also rounds the DateTime)
+overallData$DateTime <- as.POSIXct(overallData$DateTime, format = "%d.%m.%Y %H:%M:%S")
+
+# separate Animal into his ID an his system
+overallData[c('AnimalID', 'System')] <- str_split_fixed(overallData$Animal, '_', 2)
+
+
+#################### convert xPos and yPos into one column named "PositionID" ###########################################################
+
+# finds corresponding PositionID from to coordinates(x_Pos, y_pos) in lookup_tibble and returns ID 
+find_id <- function(x_Pos, y_Pos, lookup_tibble) {
+  result <- lookup_tibble %>%
+    filter(xPos == x_Pos, yPos == y_Pos) %>%
+    select(PositionID)
+  
+  if (nrow(result) > 0) {
+    return(result$PositionID)
+  } else {
+    return(NA) # Return NA if no match found
+  }
+}
+
+#Positions_tibble contains every possible combination of our coordinates together with an ID
+positions <- select(overallData, c(xPos,yPos))
+unique_positions <- unique(positions)
+Positions_tibble <- tibble(PositionID = c(1:length(unique_positions$xPos)), xPos = unique_positions[1], yPos = unique_positions[2])
+
+# Adding column PositionID to overallData instead of two colums with x and y coordinates
+overallData_ids <- overallData %>% rowwise() %>%
+  mutate(PositionID = find_id(xPos, yPos, Positions_tibble))
+
+
+##############################################################################
+
+# sort columns
+overallData_final <- overallData_ids[c('DateTime', 'AnimalID', 'System', 'PositionID')]
+#?
+overallData_final <- as_tibble(overallData_final)
+########################################################################################################
+
+# algorithm 
+
+# declare start position and start time of mouse in one system(4mice together)
+mouseOne    <- list(name="", time="")
+mouseTwo    <- list(name="", time="")
+mouseThree  <- list(name="", time="")
+mouseFour   <- list(name="", time="")
+
+mice_list <- list(
+  "mouseOne" = mouseOne,
+  "mouseTwo" = mouseTwo,
+  "mouseThree" = mouseThree,
+  "mouseFour" = mouseFour)
+
+#print the list of mice 
+for (sub_list in names(mice_list)) {
+  print(sub_list)
+  for (element in mice_list[[sub_list]]) {
+    print(element)
+  }
+}
+# save names of the first system
+mice_systemOne <- overallData_final%>%
+  filter(System == "sys.1")
+
+mouse_names_systemOne <- unique(mice_systemOne$AnimalID)
+mouse_names_systemOne[[1]]
+
+# find the first time where mouse is tracked in the cage
+# aka first value of mopuse in overallData_final
+for (i in 1:length(mouse_names_systemOne)){ #von i=1-4
+  #search in whole data
+  first_entry <- overallData_final%>%
+    filter(AnimalID == mouse_names_systemOne[[i]])%>%
+    slice(1) #erste Zeile/ filter(row_number()==1)
+    
+  first_time <- first_entry$DateTime
+  first_position <- first_entry$PositionID
+  #give time and position in list
+  print(first_time)
+  print(first_position)
+}
+
+
