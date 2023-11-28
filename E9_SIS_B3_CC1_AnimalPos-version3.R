1c6db11ef3089b053bef43f0255f2dc9480ee539
anja7777 <100579985+anja7777@users.noreply.github.com>
2023-11-28 11:41:13 +0100

    third try, new doc, preprocess framework

---
+++ E9_SIS_B3_CC1_AnimalPos-version3.R
@@ -0,0 +1,56 @@
+## 11/2023
+## Anja Magister
+## ANALYSIS OF ANIMAL POSITIONS - VERSION3 ##
+##
+
+# libraries
+library(readr)        # load readr package for reading csv files
+library(stringr)
+library(dplyr)
+library(lubridate)    # for rounding time, time operations in general
+library(tibble)       #important for tibble operations
+library(purrr)
+library(ggplot2)      #for plots
+
+# paths
+working_directory <- "S:/Lab_Member/Anja/Git/AnjaIntern"
+fileSourcePath <-  paste0(working_directory,"/E9_SIS_B3_CC1_AnimalPos.csv")
+
+#fuctions
+source(paste0(working_directory,"/E9_SIS_B3_CC1_AnimalPos-functions.R"))
+
+# read csv file in tibble
+
+overallData <- as_tibble(read_delim(fileSourcePath,delim = ";", show_col_types = FALSE))
+
+####################################################################################################################################
+###### PREPROCESSING OF overallData: ######
+
+# delete unnecessary columns
+overallData <- select(overallData, -c(RFID, AM, zPos))
+# convert the DateTime column to a datetime format(also rounds the DateTime)
+overallData$DateTime <- as.POSIXct(overallData$DateTime, format = "%d.%m.%Y %H:%M:%S")
+
+# separate Animal into his ID an his system
+overallData[c('AnimalID', 'System')] <- str_split_fixed(overallData$Animal, '_', 2)
+
+
+
+
+###### convert xPos and yPos into one column named "PositionID" ######
+
+#create Positions_tibble that contains every possible combination of our coordinates together with an ID
+positions <- select(overallData, c(xPos,yPos))
+unique_positions <- unique(positions)
+Positions_tibble <- tibble(PositionID = c(1:length(unique_positions$xPos)), xPos = unique_positions[1], yPos = unique_positions[2])
+
+# Adding column PositionID to overallData instead of two colums with x and y coordinates
+overallData_ids <- overallData %>% rowwise() %>%
+  mutate(PositionID = find_id(xPos, yPos, Positions_tibble))
+
+
+
+##### sort columns #####
+overallData <- overallData_ids[c('DateTime', 'AnimalID', 'System', 'PositionID')]
+
+################################################################################################################################
